42e31c58a38f1416e8b3deeeda2b8b5b19e397dd
Agnes Mrutu <217697350+amrutu75@users.noreply.github.com>
2025-06-26 21:47:19 -0700

    Created app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from shiny.express import ui, input, render
+
+#Add Page title
+ui.page_opts(title="PyShiny App with Plot",fillable=True)
+
+#Add sidebar with a slieder input
+with ui.sidebar():
+    ui.input_slider("selected_number_of_bins", "Number of Bins", 0, 100, 20)
+
+
+@render.plot(alt="A histogram showing random data distribution")
+def histogram():
+    np.random.seed(19680801)
+    x = 100 + 15 * np.random.randn(437)
+    plt.hist(x, input.selected_number_of_bins(), density=True)
